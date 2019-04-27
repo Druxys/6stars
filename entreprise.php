dbb5dd9c8dd7dcc83014cca252fe303cee3a3533
camillelupo <camille.lupo@hotmail.fr>
2019-04-27 11:56:08 +0200

    next

--- entreprise.php
+++ entreprise.php
@@ -11,15 +11,15 @@ include ("inc/header.php");
 ?>
 
     <div class="container-fluid">
-        <div class="row">
+        <div class="row my-3">
             <div class="col-md-3">
             </div>
             <div class="col-md-2">
                 <!-- Photo de profil-->
-                <img src="assets/img/men-1979261_640.jpg" class="img-fluid">
+                <img src="assets/img/bbird.jpg" class="img-fluid">
             </div>
             <div class="col-md-4">
-                <p>Nom de l'entreprise : BBIRD</p>
+                <p class="h3">Nom de l'entreprise : BBIRD</p>
                 <p>Secteur : Communication</p>
                 <p>Site web : www.bbird.com</p>
                 <p>Téléphone : 02 36 67 05 87</p>
@@ -29,10 +29,10 @@ include ("inc/header.php");
             </div>
         </div>
 
-        <div class="row">
+        <div class="row my-3">
             <div class="col-md-3">
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 text-center">
                 <!-- Score entreprise -->
                 <h3>Score entreprise</h3>
                 <i class="fas fa-star fa-2x"></i>
@@ -58,13 +58,14 @@ include ("inc/header.php");
             </div>
         </div>
         <br>
-        <div class="row">
+        <div class="row my-3">
             <div class="col-md-3">
             </div>
             <br>
             <div class="col-md-2">
+                <h3>Score</h3>
                 <!-- Note individuelle -->
-                <h5>Score Recrutement</h5>
+                <h5>Recrutement</h5>
                 <i class="fas fa-star"></i>
                 <i class="fas fa-star "></i>
                 <i class="fas fa-star "></i>
@@ -72,7 +73,7 @@ include ("inc/header.php");
                 <i class="far fa-star "></i>
                 <i class="far fa-star "></i>
                 <br>
-                <h5>Score Poste de travail</h5>
+                <h5>Poste de travail</h5>
                 <i class="fas fa-star "></i>
                 <i class="fas fa-star "></i>
                 <i class="fas fa-star "></i>
@@ -84,18 +85,25 @@ include ("inc/header.php");
             <div class="col-md-2">
                 <!-- Actualités -->
                 <h3>Actualités</h3>
-                <section class="">
-                    <p>ON MET LES TEXTES DE SES MORTS</p>
-                </section>
+                <p>27/04/2019 : <br> Participation au Handihack 2019</p>
             </div>
             <div class="col-md-2">
                 <!-- Commentaire + / - -->
-                <div class="row">
-                    <h4>Commentaire <i class="fas fa-plus-circle"></i></h4>
-                </div>
-                <br>
-                <div class="row">
-                    <h4>Commentaire <i class="fas fa-minus-circle"></i></h4>
+                <h3>Commentaires</h3>
+
+                <div class="row my-3 text-center ">
+
+                    <div class="text-center">
+                        <h4><i class="fas fa-plus-circle"></i></h4>
+                        <p class="ml-3">Ils sont trop sympas </p>
+                    </div>
+
+
+                    <div class="text-center">
+                        <h4><i class="fas fa-minus-circle"></i></h4>
+                        <p class="ml-3">C'est pas très adapté </p>
+                    </div>
+
                 </div>
 
             </div>
@@ -105,9 +113,9 @@ include ("inc/header.php");
         <div class="row text-center">
             <div class="col-md-3">
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 mb-5">
                 <!-- Voir plus de détails -->
-                <h2><a href="#">Voir plus de détails</a></h2>
+                <h2><a href="#" class="btn btn-dark btn-lg">Voir plus de détails</a></h2>
             </div>
             <div class="col-md-3">
             </div>
