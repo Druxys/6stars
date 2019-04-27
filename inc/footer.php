15607c1317e3280165d7dfdbb691d5b959dbf157
Jonathan <jon.renault@protonmail.com>
2019-04-27 05:25:05 +0200

    header footer index

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,73 @@
+<footer>
+        <div class="row bg-dark text-white py-4">
+            <div class="col-md-3">
+                <ul class="list-link">
+                    <li class="list-item">
+                       <a class="link-style" href="#">Accueil</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="link-style" href="#">Chercher une entreprise</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="link-style" href="#">Donner mon avis</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="link-style" href="#">S'inscrire</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="link-style" href="#">Se connecter</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h3 class="text-center">
+                    Entreprise
+                </h3>
+                <ul class="list-link">
+                    <li class="list-item">
+                       Espace pro
+                    </li>
+                    <li class="list-item">
+                        Les conseils RH
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h3 class="text-center">
+                6-stars
+                </h3>
+                <ul class="list-link">
+                    <li class="list-item">
+                       A propos de nous
+                    </li>
+                    <li class="list-item">
+                        Le Classement 6-stars
+                    </li>
+                    <li class="list-item">
+                        Foire aux questions
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h3>
+                    Nous contacter
+                </h3>
+                <address>
+                    <strong>6-stars, Inc.</strong><br />24 place Saint Marc<br /> 76000 Rouen<br /> <abbr title="Phone">P:</abbr> 02.35.70.04.04<br /> <abbr title="Mail">M:</abbr> bonjour@6stars.com
+                </address>
+                <div class="social-networks" role="group">
+                    <a class="mx-2" href="#"><i class="fab fa-facebook fa-2x"></i></a>
+                    <a class="mx-2" href="#"><i class="fab fa-twitter fa-2x"></i></a>
+                    <a class="mx-2" href="#"><i class="fab fa-instagram fa-2x"></i></a>
+
+                </div>
+            </div>
+        </div>
+</footer>
+
+
+
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
