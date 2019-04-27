2288670640e842688d02111c81375122262af07c
camillelupo <camille.lupo@hotmail.fr>
2019-04-27 11:10:07 +0200

    front page entreprise

--- entreprise.php
+++ entreprise.php
@@ -7,26 +7,113 @@
  */
 
 
-include("inc/header.php");
+include ("inc/header.php");
 ?>
 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+            <div class="col-md-2">
+                <!-- Photo de profil-->
+                <img src="assets/img/men-1979261_640.jpg" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <p>Nom de l'entreprise : BBIRD</p>
+                <p>Secteur : Communication</p>
+                <p>Site web : www.bbird.com</p>
+                <p>Téléphone : 02 36 67 05 87</p>
+                <!-- Info profil-->
+            </div>
+            <div class="col-md-3">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+            <div class="col-md-3">
+                <!-- Score entreprise -->
+                <h3>Score entreprise</h3>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="far fa-star fa-2x"></i>
+                <i class="far fa-star fa-2x"></i>
+            </div>
+            <div class="col-md-3">
+                <!-- Score user -->
+                <h3>Score utilisateur</h3>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="fas fa-star fa-2x"></i>
+                <i class="far fa-star fa-2x"></i>
+                <i class="far fa-star fa-2x"></i>
+                <i class="far fa-star fa-2x"></i>
 
 
+            </div>
+            <div class="col-md-3">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+            <br>
+            <div class="col-md-2">
+                <!-- Note individuelle -->
+                <h5>Score Recrutement</h5>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star "></i>
+                <i class="fas fa-star "></i>
+                <i class="far fa-star "></i>
+                <i class="far fa-star "></i>
+                <i class="far fa-star "></i>
+                <br>
+                <h5>Score Poste de travail</h5>
+                <i class="fas fa-star "></i>
+                <i class="fas fa-star "></i>
+                <i class="fas fa-star "></i>
+                <i class="far fa-star "></i>
+                <i class="far fa-star "></i>
+                <i class="far fa-star "></i>
+                <br>
+            </div>
+            <div class="col-md-2">
+                <!-- Actualités -->
+                <h3>Actualités</h3>
+                <section class="">
+                    <p>ON MET LES TEXTES DE SES MORTS</p>
+                </section>
+            </div>
+            <div class="col-md-2">
+                <!-- Commentaire + / - -->
+                <div class="row">
+                    <h4>Commentaire <i class="fas fa-plus-circle"></i></h4>
+                </div>
+                <br>
+                <div class="row">
+                    <h4>Commentaire <i class="fas fa-minus-circle"></i></h4>
+                </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            </div>
+            <div class="col-md-3">
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-3">
+            </div>
+            <div class="col-md-6">
+                <!-- Voir plus de détails -->
+                <h2><a href="#">Voir plus de détails</a></h2>
+            </div>
+            <div class="col-md-3">
+            </div>
+        </div>
+    </div>
 
 <?php
 
-include("../inc/footer.php");
+include ("inc/footer.php");
