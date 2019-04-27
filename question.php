216d5fccd77d70ff1dbbff2011cef39063e1fb93
camillelupo <camille.lupo@hotmail.fr>
2019-04-27 06:27:49 +0200

    Avis front

--- question.php
+++ question.php
@@ -28,29 +28,48 @@ include ('inc/header.php');
 
 ?>
 
+<div class="row my-4 note">
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-8 text-center page-header">
+        <h2>Donner mon avis sur une entreprise</h2>
+        <p>
+            Contribuez en renseignant votre expérience au sein de cette entreprise.
+        </p>
+    </div>
+    <div class="col-md-2">
+    </div>
+</div>
 
-<form class="" action="" method="post">
-
-    <?php
-
-        foreach ($tableaux as $tableau){
-            ?>
-            <label><?php echo $tableau['question'] ?></label>
-            <input type="text" name="reponse">
-            <label>note</label>
-            <input type="integer" name="note">
-            <br>
+<div class="row my-4">
+    <div class="col-md-4">
+        <img src="assets/img/dylan-gillis-533818-unsplash.jpg" alt="Personnes travaillant sur une table" style="width:inherit;">
+    </div>
+    <div class="col-md-8">
+        <form class="" action="" method="post">
 
             <?php
 
-        }
-?>
+                foreach ($tableaux as $tableau){
+                    ?>
+                    <label><?php echo $tableau['question'] ?></label>
+                    <?php br(); ?>
+                    <input type="text" name="reponse">
 
-    <input type="submit" name='submitted'>
+                    <label>note</label>
+                    <input type="number" name="note">
+                    <br>
 
+                    <?php
 
+                }
+        ?>
 
-</form>
+            <input type="submit" name='submitted'>
+
+        </form>
+    </div>
+</div>
 
 
 
